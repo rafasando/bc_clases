876300f614bc67ff50800f09fad779d86a97f521
El Arquitecto <sandoval.rafael@gmail.com>
2019-04-25 00:08:56 +0200

    Cumplir anhos

--- Persona.py
+++ Persona.py
@@ -1,17 +1,37 @@
 # En el archivo Persona.py crear una clase Persona con atributo nombre. 
-# Despues, instanciar un objeto de tipo Persona
+# Despues, instanciar un objeto de tipo Persona 
+# Agregarle un atributo edad y un metodo cumple_anhos 
+# y un metodo get_edad. 
+# Inicializar un objeto de tipo Persona y hacerle cumplir anhos
 
 class Persona:
     nombre = None
+    edad = None
 
-    def __init__(self, arg_nombre):
-        set_Nombre(arg_nombre)
+    def __init__(self, arg_nombre, arg_edad):
+        self.set_Nombre(arg_nombre)
+        self.set_Edad(arg_edad)
 
     def get_Nombre(self):
         return self.nombre
-
     def set_Nombre(self, arg_nombre):
         self.nombre = arg_nombre
 
-p = Persona("Rrrrr")
-print(p.nombre)
+    def get_Edad(self):
+        return self.edad
+    def set_Edad(self, arg_edad):
+        self.edad = arg_edad
+
+    def cumple_anhos(self):
+        self.set_Edad(self.get_Edad() + 1)
+
+from time import sleep
+
+p = Persona("Ror", 10)
+print("Te llamas", p.nombre, "y tenes", p.edad, "anhos")
+for i in range(5):
+    print("Ahora quiero que cumplas un anho mas!!!")
+    p.cumple_anhos()
+    print("Edad:", p.edad)
+    sleep(2)
+
