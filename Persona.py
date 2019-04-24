89c18381b47a51363248cceef7ab0fcb2e8edc15
El Arquitecto <sandoval.rafael@gmail.com>
2019-04-24 22:49:19 +0200

    Se cambio la definicion de persona como clase

--- Persona.py
+++ Persona.py
@@ -1,4 +1,17 @@
-# Esto es una prueba
-print("Que tal Mundo?")
-print("Chauuuuuuuuu")
-print("Un mensaje")
+# En el archivo Persona.py crear una clase Persona con atributo nombre. 
+# Despues, instanciar un objeto de tipo Persona
+
+class Persona:
+    nombre = None
+
+    def __init__(self, arg_nombre):
+        set_Nombre(arg_nombre)
+
+    def get_Nombre(self):
+        return self.nombre
+
+    def set_Nombre(self, arg_nombre):
+        self.nombre = arg_nombre
+
+p = Persona("Rrrrr")
+print(p.nombre)
